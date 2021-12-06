436db2d665c31d87ccaea8bcdaf0d3c3ef256239
Muhammad Touseef <muhammadtouseefmughal@gmail.com>
2021-12-06 18:00:03 +0500

    Auth Added

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -2,29 +2,31 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap";
 import "react-bootstrap-drawer/lib/style.css";
 import { Dashboard } from "./components/layout/Dashboard";
-import {Navbar,Container} from 'react-bootstrap'
-import './App.css'
+import { Navbar, Container } from "react-bootstrap";
+import "./App.css";
 import Topbar from "./components/layout/Topbar";
-import {
-  BrowserRouter,
-  Routes,
-  Route
-} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Login } from "./components/pages/Login";
+import AuthState from "./context/auth/AuthState";
 function App() {
   return (
-  
-  <BrowserRouter>
-   
-   <Routes>
-     <Route path="/login" element={<Topbar/>} />
-      <Route path="/" element={<Dashboard> <Topbar/> </Dashboard>}>
-        <Route path="abc" element={<Topbar/> } />
-     
-      </Route>
-    </Routes>
-  </BrowserRouter>
-
-    
+    <AuthState>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route
+            path="/"
+            element={
+              <Dashboard>                
+                <Topbar />
+              </Dashboard>
+            }
+          >
+            <Route path="abc" element={<Topbar />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </AuthState>
   );
 }
 
