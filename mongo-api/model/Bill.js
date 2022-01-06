0736515a3a9e2d552d302dcf19037c41cd5cf438
Muhammad Touseef <muhammadtouseefmughal@gmail.com>
2022-01-06 23:06:24 +0500

    Final Commit

--- mongo-api/model/Bill.js
+++ mongo-api/model/Bill.js
@@ -1,29 +1,24 @@
-const crypto = require('crypto');
-const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
+const crypto = require("crypto");
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 var Schema = mongoose.Schema;
 const BillSchema = new mongoose.Schema({
-    patientid: { type: Schema.Types.ObjectId, ref: 'PATIENT' },
-    Tests :[{type:mongoose.Schema.Types.ObjectId,ref: 'TEST'}]
-    ,
-    samples:{
-        type: Array
-    }
-    ,
-    results:{
-        type: Array
-    },
-    
-    billid: Number ,
-      cost: Number,
-      tax: Number,
-      grandTotal: Number,
-      password: String,
-   
-  });
+  patientid: { type: Schema.Types.ObjectId, ref: "PATIENT" },
+  Tests: [{ type: mongoose.Schema.Types.ObjectId, ref: "TEST" }],
+  samples: {
+    type: Array,
+  },
+  results: {
+    type: Array,
+  },
 
+  billid: Number,
+  cost: Number,
+  tax: Number,
+  grandTotal: Number,
+  password: String,
+});
 
-  
-module.exports = mongoose.model('BILL', BillSchema);
+module.exports = mongoose.model("BILL", BillSchema);
